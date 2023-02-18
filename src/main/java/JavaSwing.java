124201bd7ba20c055c50ed3664ff029d4cf92b24
Srithar <sritharboss8@gmail.com>
2023-02-18 16:03:47 +0530

    Moved to Git

--- src/main/java/JavaSwing.java
+++ src/main/java/JavaSwing.java
@@ -22,12 +22,12 @@ import javax.swing.SwingUtilities;
 
 public class JavaSwing implements ActionListener, MouseListener {
 
-	private JFrame frame;
-	private ButtonGroup group;
-	private JRadioButton button1, button2;
-	private JTextArea area, area1, area2;
-	private JTextField field;
-	private JButton button;
+	JFrame frame;
+	ButtonGroup group;
+	JRadioButton button1, button2;
+	JTextArea area, area1;
+	JTextField field;
+	JButton button;
 
 	private void build() {
 		frame = new JFrame("Encryption & Decryption");
@@ -60,7 +60,7 @@ public class JavaSwing implements ActionListener, MouseListener {
 		});
 
 		area = new JTextArea("Enter Your Text to Decrypt");
-		area.setBounds(20, 60, 150, 30);
+		area.setBounds(20, 60, 160, 30);
 		area.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
 		area.setEditable(false);
 		area.setBackground(null);
@@ -80,8 +80,10 @@ public class JavaSwing implements ActionListener, MouseListener {
 
 		area1.addMouseListener(this);
 
+		
+		
 		SwingUtilities.invokeLater(new Runnable() {
-
+			
 			@Override
 			public void run() {
 				JLabel label = new JLabel(new ImageIcon(getClass().getResource("/main/java/p.jpg")));
@@ -89,8 +91,8 @@ public class JavaSwing implements ActionListener, MouseListener {
 				frame.add(label);
 			}
 		});
-
-		area2 = new JTextArea("Tip : Right Click on the Text Area to Copy the Output");
+		
+		JTextArea area2 = new JTextArea("Tip : Right Click on the Text Area to Copy the Output");
 		area2.setBounds(40, 320, 340, 20);
 		area2.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 12));
 		area2.setEditable(false);
@@ -116,21 +118,20 @@ public class JavaSwing implements ActionListener, MouseListener {
 	public void actionPerformed(ActionEvent e) {
 
 		if (button1.isSelected()) {
-			area1.setText("Decrypted -->" + field.getText());
+			area1.setText("Decrypt the text \""+field.getText()+"\"");
 		}
 
 		if (button2.isSelected()) {
-			area1.setText("Encrypted -->" + field.getText());
+			area1.setText("Encrypt the text \""+field.getText()+"\"");
 		}
 
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON3 && !area1.getText().equals("")) {
+		if (e.getButton() == MouseEvent.BUTTON3) {
 			Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
 			clip.setContents(new StringSelection(area1.getText()), null);
-			area2.setText("Content Copied to Clipboard....");
 		}
 
 	}
